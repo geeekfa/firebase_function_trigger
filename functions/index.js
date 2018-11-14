8ed15d92ee74f41fe1988362c89621b433dd5fe7
majidi <geeekfa@gmail.com>
2018-11-14 17:02:59 +0330

    first commit

---
+++ functions/index.js
@@ -0,0 +1,27 @@
+const functions = require('firebase-functions');
+const firebaseAdmin = require('firebase-admin');
+var serviceAccount = require('./path/to/serviceAccountKey.json');
+firebaseAdmin.initializeApp({
+    credential: firebaseAdmin.credential.cert(serviceAccount),
+    databaseURL: "https://university-20366.firebaseio.com"
+});
+const fireStore = firebaseAdmin.firestore();
+const setting = { timestampsInSnapshots: true };
+fireStore.settings(setting);
+
+exports.addStudent = functions.https.onRequest((request, response) => {
+    const name = request.query.name;
+    const family = request.query.family;
+    return fireStore.collection('student').add(
+        {
+            name: name,
+            family: family
+        }
+    ).then((writeResult) => {
+        return response.json(
+            {
+                result:`${writeResult.id}`
+            }
+        );
+    });
+});
